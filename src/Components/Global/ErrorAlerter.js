7f32a15033a149c97acdf568efce243fb0f254f5
Adam Zhu <adam@adamz.hu>
2020-06-16 13:44:41 -0400

    get rid of useless ion-content

--- src/Components/Global/ErrorAlerter.js
+++ src/Components/Global/ErrorAlerter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { IonModal, IonButton, IonContent } from "@ionic/react";
+import { IonModal, IonButton } from "@ionic/react";
 import "./ErrorAlerter.css";
 import { clear_app_errors } from "Store/errors/thinks";
 
@@ -13,21 +13,19 @@ const ErrorAlerter = () => {
   };
 
   return errors.length === 0 ? null : (
-    <IonContent>
-      <IonModal isOpen cssClass="error-alerter">
-        <form onSubmit={dismiss_handler}>
-          <h1 className="header">
-            <i className="material-icons">error</i> Error
-          </h1>
-          <ul>
-            {errors.map((message) => (
-              <li>{message}</li>
-            ))}
-          </ul>
-          <IonButton type="submit">OK</IonButton>
-        </form>
-      </IonModal>
-    </IonContent>
+    <IonModal isOpen cssClass="error-alerter">
+      <form onSubmit={dismiss_handler}>
+        <h1 className="header">
+          <i className="material-icons">error</i> Error
+        </h1>
+        <ul>
+          {errors.map((message) => (
+            <li>{message}</li>
+          ))}
+        </ul>
+        <IonButton type="submit">OK</IonButton>
+      </form>
+    </IonModal>
   );
 };
 
