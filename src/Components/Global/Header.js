7b49fce0c0fd547d73ba9456784b90b12a7fc3c0
Adam Zhu <adam@adamz.hu>
2020-09-17 17:26:28 -0400

    push prod

--- src/Components/Global/Header.js
+++ src/Components/Global/Header.js
@@ -14,6 +14,8 @@ const Header = () => {
   const location = useLocation();
   const log_out_handler = () => dispatch(log_user_out(history));
 
+  console.log(location);
+
   return (
     <IonHeader>
       <IonToolbar>
